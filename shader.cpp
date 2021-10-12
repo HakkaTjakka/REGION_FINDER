9d30f227e67c35b9b8a5b835c37b2cb9e098ab48
Pacman Graphics <hakkatjakka66@gmail.com>
2021-10-12 05:36:30 +0200

    Upd.

---
+++ shader.cpp
@@ -0,0 +1,4 @@
+ glUniform1i(glGetUniformLocation(prog_id,"txr_font"),0);
+ glUniform1f(glGetUniformLocation(prog_id,"fxs"),(8.0)/float(xs));
+ glUniform1f(glGetUniformLocation(prog_id,"fys"),(8.0)/float(ys));
+ 
